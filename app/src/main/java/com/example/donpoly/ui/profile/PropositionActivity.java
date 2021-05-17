94f02540c0a30e120edd104ec221ffa321e0fe98
Clemence-jzq <zheqin.jiang@gmail.com>
2021-05-17 14:45:24 +0200

    Update PropositionActivity.java

--- app/src/main/java/com/example/donpoly/ui/profile/PropositionActivity.java
+++ app/src/main/java/com/example/donpoly/ui/profile/PropositionActivity.java
@@ -1,10 +1,12 @@
 package com.example.donpoly.ui.profile;
 
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
 
@@ -14,6 +16,7 @@ import com.example.donpoly.data.tools.Status;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.IntStream;
 
 public class PropositionActivity extends AppCompatActivity {
     private RecyclerView mRecyclerView;
@@ -27,6 +30,22 @@ public class PropositionActivity extends AppCompatActivity {
         
         initData();
         initView();
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.N)
+    private void listen() {
+        while (true){
+            Intent intent = getIntent();
+            String name = intent.getStringExtra("namePoly");
+            //remove the item from list
+            IntStream.range(0,propositionList.size()).filter(i->propositionList.get(i).getTitle().equals(name)).boxed().findFirst().map(i->propositionList.remove((int)i));
+            myRecyclerViewAdapter.notifyDataSetChanged();
+            try {
+                Thread.sleep(5000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
     }
 
 
